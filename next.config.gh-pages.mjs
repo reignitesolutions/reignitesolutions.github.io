ee5ce93552aa47ad5e61b447def596745122d211
reignitesolutions <helloalexhunter@gmail.com>
2025-10-20 14:11:20 +0100

    Full Delete

--- next.config.gh-pages.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This tells Next.js to export a static site.
-  output: "export",
-  basePath: "/reignitesolutions.github.io"
-};
-
-export default nextConfig;
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This is a standard Next.js configuration.
-  // We have removed the 'output' and 'basePath' properties.
-  // This will allow `npm run build` to create a standard production build.
-};
-
-export default nextConfig;
